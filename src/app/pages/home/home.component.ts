4846b45fb7b4290d5f2113cff565d0d6e0006430
Henrique Melo <hmelo@cgee.org.br>
2021-10-09 14:53:17 -0300

    Slide de imagens na pagina home e ajustes no footer + navbar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,12 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  imageNumber: number = 1;
+  interval: any;
+
   constructor() { }
 
   ngOnInit(): void {
     const filler = document.getElementById("fillerHome") as HTMLElement
-    if( window.innerWidth >= 1001) filler.style.height = "calc(100vh - 140px)"
+    if( window.innerWidth >= 1001) filler.style.height = "calc(100vh - 150px)"
     else filler.style.height = "calc(100vh - 60px)"
+    this.startSlideShow()
+  }
+
+  skipImage() {
+    if(this.imageNumber === 4) this.imageNumber = 1;
+    else this.imageNumber = this.imageNumber + 1;
+    clearInterval(this.interval);
+    this.startSlideShow()
+  }
+
+  backImage() {
+    if(this.imageNumber === 1) this.imageNumber = 4;
+    else this.imageNumber = this.imageNumber - 1;
+    clearInterval(this.interval);
+    this.startSlideShow()
+  }
+
+  startSlideShow(){
+    this.interval = setInterval( ()=> {
+      if(this.imageNumber === 4) this.imageNumber = 1;
+      else this.imageNumber = this.imageNumber + 1;
+    }, 5000);
   }
 
 }
