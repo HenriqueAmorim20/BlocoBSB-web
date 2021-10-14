557a91bf9a9d3a401cbd03f4dd9092f0bab8db8d
Henrique Melo <hmelo@cgee.org.br>
2021-10-14 19:10:45 -0300

    instalação de uma store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxsModule } from '@ngxs/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,6 +9,8 @@ import { MaterialModule } from './material.module';
 import { PagesModule } from './pages/pages.module';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './@shared/shared.module';
+import { environment } from '../environments/environment';
+import { AuthStore } from './@state/login.store';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { SharedModule } from './@shared/shared.module';
     MaterialModule,
     PagesModule,
     LayoutModule,
-    SharedModule
+    SharedModule,
+    NgxsModule.forRoot([AuthStore],  {
+        developmentMode: !environment.production
+      })
   ],
   providers: [],
   bootstrap: [AppComponent]
