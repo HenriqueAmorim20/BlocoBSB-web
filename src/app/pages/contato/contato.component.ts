a59df792b206a87a2c6223601b23b0fb1bc242c8
Henrique Melo <hmelo@cgee.org.br>
2021-10-17 16:08:41 -0300

    Enviar mensagem implementado com tratamento de notificações

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Select } from '@ngxs/store';
+import { NotificationsService } from 'angular2-notifications';
+import { AppService } from 'src/app/services/app.service';
+import { CredentialsService } from 'src/app/services/credentials.service';
 
 @Component({
   selector: 'app-contato',
@@ -15,19 +20,47 @@ export class ContatoComponent implements OnInit {
     mensagem: new FormControl('', [Validators.required, Validators.minLength(3)])
   });
 
-  constructor() { }
+  logado: boolean = false;
+  @Select((state: any) => state.login) stateLogin: any;
+
+  constructor(private credentialService: CredentialsService, private appService: AppService,  private notification: NotificationsService, private router: Router) { }
 
   ngOnInit(): void {
-    // const filler = document.getElementById("fillerContato") as HTMLElement
-    // if( window.innerWidth >= 1001) filler.style.height = "calc(100vh - 140px)"
-    // else filler.style.height = "calc(100vh - 60px)"
-    // const y = filler.getBoundingClientRect().height + 60
-    // setTimeout(()=>{window.scrollTo({top: y, behavior: 'smooth'})},1000)
+    this.stateLogin.subscribe(async (res: any) => {
+      this.logado = res.email? true : false
+      if(this.logado) {
+        const user = this.credentialService.credentials
+        this.contatoForm.patchValue({email: user?.email})
+        this.contatoForm.patchValue({nome: user?.nome})
+      }
+    });
   }
 
-  enviarMensagem(){
-    if(this.contatoForm.valid)
-      console.log(this.contatoForm.value)
+  async enviarMensagem(){
+    if(this.contatoForm.valid) {
+      try {
+        const result = await this.appService.sendFeedback(this.contatoForm.value).toPromise()
+        this.notification.success('Sucesso!', 'Obrigado pelo seu contato, sua mensagem será analisada em breve.', {
+          timeOut: 5000,
+          showProgressBar: true,
+          pauseOnHover: true,
+        })
+        this.router.navigate(['/home'])
+        return
+      } catch (error) {
+        this.notification.error('Erro!', 'Não foi possível mandar sua mensagem.', {
+          timeOut: 5000,
+          showProgressBar: true,
+          pauseOnHover: true,
+        })
+        return
+      }
+    }
+    this.notification.info('Atenção!', 'Campos inválidos.', {
+      timeOut: 5000,
+      showProgressBar: true,
+      pauseOnHover: true,
+    })
   }
 
 }
