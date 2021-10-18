f812e5386dc230e6a39f510c5592204d65adec24
Henrique Melo <hmelo@cgee.org.br>
2021-10-18 09:26:12 -0300

    newsletter

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { NotificationsService } from 'angular2-notifications';
 import { ModalTabelaTamanhosComponent } from 'src/app/@shared/components/modal-tabela-tamanhos/modal-tabela-tamanhos.component';
 import { ModalTrocasComponent } from 'src/app/@shared/components/modal-trocas/modal-trocas.component';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-footer',
@@ -13,11 +16,40 @@ export class FooterComponent implements OnInit {
   @Input() innerWidth: any;
   data: any;
 
-  constructor(public dialog: MatDialog) { }
+  newsletterForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required ])
+  });
+
+  constructor(public dialog: MatDialog, private notification: NotificationsService, private service: AppService) { }
 
   ngOnInit(): void {
     const currentdate = new Date();
     this.data = currentdate.getFullYear();
+  }
+
+  async addNewsletter(){
+    if(!this.newsletterForm.valid) {
+      this.notification.info('Atenção!', 'Email inválido.', {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: true,
+      })
+      return
+    }
+    const result = await this.service.addNewsletter({email: this.newsletterForm.value.email}).toPromise()
+    if(result._id) {
+      this.notification.success('Sucesso!', `Você recebera novidades dos nosso produtos.`, {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: true,
+      })
+    } else {
+      this.notification.error('Erro!', 'Não foi possível te cadastrar na newsletter, email existente.', {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: true,
+      })
+    }
   }
 
   modalTrocas(){
