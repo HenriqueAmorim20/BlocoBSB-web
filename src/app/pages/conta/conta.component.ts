af1f014ccbaf01ef49633280c214d43e785e8259
Henrique Melo <hmelo@cgee.org.br>
2021-10-20 21:01:31 -0300

    Ajustes na página conta e patch de dados do usuario

--- src/app/pages/conta/conta.component.ts
+++ src/app/pages/conta/conta.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Select } from '@ngxs/store';
 import { NotificationsService } from 'angular2-notifications';
 import { CredentialsService } from 'src/app/services/credentials.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-conta',
@@ -11,17 +13,30 @@ import { CredentialsService } from 'src/app/services/credentials.service';
 })
 export class ContaComponent implements OnInit {
 
-  nome: string = ''
-  email: string = ''
   @Select((state: any) => state.login) stateLogin: any;
+  user: any;
 
-  constructor(private credentialService: CredentialsService, private router: Router, private notification: NotificationsService) { }
+  contaForm: FormGroup = new FormGroup({
+    nome: new FormControl('', [Validators.required]),
+    telefone: new FormControl('', []),
+  });;
+
+  constructor(private service: LoginService, private credentialService: CredentialsService, private router: Router, private notification: NotificationsService) { }
 
   ngOnInit(): void {
     this.stateLogin.subscribe((res: any) => {
-      this.email = res.email
+      if(res.email) this.getInfo(res.email)
     })
-    this.nome = this.credentialService.credentials?.nome
+  }
+
+  getInfo(email: any){
+    this.service.getUserByEmail(email).subscribe( (res:any) => {
+      this.user = res[0]
+      this.contaForm = new FormGroup({
+        nome: new FormControl(this.user.nome, [Validators.required]),
+        telefone: new FormControl(this.user.telefone, []),
+      });
+    });
   }
 
   logout() {
@@ -31,6 +46,41 @@ export class ContaComponent implements OnInit {
       showProgressBar: true,
       pauseOnHover: true,
     })})
+  }
+
+  async atualizarDados(){
+    if(this.user.nome === this.contaForm.value.nome && this.user.telefone === this.contaForm.value.telefone){
+      this.notification.info('Atenção!', 'Nada a ser alterado.', {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: true,
+      })
+      return
+    }
+    if(!this.contaForm.valid) {
+      this.notification.info('Atenção!', 'Campos inválidos.', {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: true,
+      })
+      return
+    }
+    const result = await this.service.patchUser(this.user._id, [{ chave: 'nome', valor: this.contaForm.value.nome }, { chave: 'telefone', valor: this.contaForm.value.telefone }]).toPromise();
+    if(result._id) {
+      this.notification.success('Sucesso!', `Dados alterados.`, {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: true,
+      })
+      this.getInfo(this.user.email)
+      return
+    }
+
+    this.notification.error('Erro!', 'Não foi possível atualizar seus dados.', {
+      timeOut: 5000,
+      showProgressBar: true,
+      pauseOnHover: true,
+    })
   }
 
 }
