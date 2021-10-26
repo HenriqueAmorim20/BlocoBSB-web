4f6e8b636b7b4067daa124ddc1ae83829f86b81f
Henrique Melo <hmelo@cgee.org.br>
2021-10-26 09:47:27 -0300

    env definitivo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://54.161.180.171/api',
+  serverUrl: 'https://blocobsb.com.br/api',
   admin: 'blocobsb@gmail.com',
   clientIdGoogle: '867953482086-olqhdfju6id9sn830710ns20r88m10b5.apps.googleusercontent.com',
 };
